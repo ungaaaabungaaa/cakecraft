0342fbd19c097f0d98090ee0a864820599844dd2
syed <syed@kemuri.in>
2025-03-21 16:28:48 +0530

    dummy placeholder adeed

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <img className="m-0 p-0" width={72} height={72} src="/1.png"/>
-        <h1 className="font-bold text-2xl md:text-4xl lg:text-7xl">CakeCraft</h1>
+    <>
+    <div className="max-w-screen max-h-screen h-screen p-0 m-0 flex items-center justify-center flex-col">
+    <img className="m-0 p-0" width={220} height={220} src="/1.png"/>
+    <h1 className="font-bold text-2xl md:text-4xl lg:text-6xl">CakeCraft</h1>
+    <h2 className="text-base font-bold">Shape Your Sweet Dream</h2>
+    <br></br>
+    <p className="text-sm font-bold">Coming Soon</p>
     </div>
+    </>
   );
 }
