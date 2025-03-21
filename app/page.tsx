47e9e2ea564b0c8bd4a1ff8637d542c53c9b66e1
syed <syed@kemuri.in>
2025-03-21 16:22:24 +0530

    updating static resources

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start text-center">
+      <img className="m-0 p-0" width={72} height={72} src="/1.png"/>
         <h1 className="font-bold text-2xl md:text-4xl lg:text-7xl">CakeCraft</h1>
-      </main>
     </div>
   );
 }
